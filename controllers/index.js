97b020ec120c4cae71dca58c1af10a048708bfaf
Marcel Melo <marcel.msmelo@gmail.com>
2017-11-24 14:19:58 -0200

    try fix bug

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    getContatinhos: (req, res) => {
+    getContatinhos: (req, res, next) => {
         Contatinho.findAll().then(contatinhos => {
             res.status(200).json(contatinhos);
         }).catch(error => {
@@ -7,21 +7,21 @@ module.exports = {
         });
 
     },
-    getContatinhoByID: (req, res) => {
+    getContatinhoByID: (req, res, next) => {
         Contatinho.findById(req.param.id).then(contatinho => {
             res.status(200).json(contatinho);
         }).catch(error => {
             res.status(500).json(null);
         });
     },
-    getContatinhoByName: (req, res) => {
+    getContatinhoByName: (req, res, next) => {
         Contatinho.findById(req.param.nome).then(contatinho => {
             res.status(200).json(contatinho);
         }).catch(error => {
             res.status(500).json(null);
         });
     },
-    createContatinho: (req, res) => {
+    createContatinho: (req, res, next) => {
         const contact = req.body;
         Contatinho.create(contact).then(contatinho => {
             res.status(201).json(null);
