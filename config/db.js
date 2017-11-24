13fd0306ceefd17c1faf05202313c5fc6b813484
Marcel Melo <marcel.msmelo@gmail.com>
2017-11-24 18:24:56 -0200

    change db connection

--- config/db.js
+++ config/db.js
@@ -1,26 +1,3 @@
-const Sequelize = require('sequelize');
-// module.exports = new Sequelize('d4sqn9dnjgu5fe', 'ffvuzjotzyakua', '0f30457546a7ce6927cfb34bd59182f383e947ff81aa5d6661e4ac34664801b7', {
-//     host: 'ec2-174-129-15-251.compute-1.amazonaws.com',
-//     dialect: 'postgres',
-//     protocol: 'postgres',
-//     port: 5432,
-//     logging: false,
-//     dialectOptions: {
-//         ssl: true
-//     }
-// });
-
-// const Sequelize = require('sequelize');
-// module.exports = new Sequelize('contatinhos', 'postgres', 'admin', {
-//     host: 'localhost',
-//     dialect: 'postgres',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         idle: 10000
-//     }
-// });
-
 let match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 module.exports = new Sequelize(match[5], match[1], match[2], {
     dialect: 'postgres',
@@ -31,4 +8,4 @@ module.exports = new Sequelize(match[5], match[1], match[2], {
     dialectOptions: {
         ssl: true
     }
-}); //
+});
